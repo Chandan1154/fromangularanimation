9b36a25626ff17847acd79517dca5f3c8e69de0f
Chandan1154 <chandan18cse@gmail.com>
2024-01-02 21:01:19 +0530

    host-context

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { Course } from '../module/Course.model';
 
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
-  styleUrl: './course.component.css'
+  styleUrl: './course.component.css',
+  // encapsulation: ViewEncapsulation.Emulated,
+  // encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.ShadowDom,
+  //  encapsulation: ViewEncapsulation.Emulate,
+  //if the no decorative there in the  component the encapsulation it by default taking as the emulated
+  // that's why Shadow dom will be used  to decorative porous
 })
 export class CourseComponent {
-
-  @Input() course!: Course ;
-
+  @Input() course!: Course;
 }
+
+// no emulates  shadow dom and net to  shadow dom
+// the emulated shadow dom which is used to the give the shadow properties
