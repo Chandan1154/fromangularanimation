e5c8efcebf83ca997035bd23abc7719b9cc40add
Chandan1154 <chandan18cse@gmail.com>
2024-01-03 13:56:02 +0530

    renderer2 commit

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-author',
@@ -7,12 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AuthorComponent {
   // isActive : string  = "is_active"
-  isActive: boolean = false;
+  isActive: boolean = true;
+  // @ViewChild('authorRef')
+  // authorElement!: ElementRef<HTMLDivElement>;
+
+  @ViewChild('authorRef', { static: true })
+  authorElement!: ElementRef<HTMLDivElement>;
+
+  // correct way to change the dom element
+  constructor(private render: Renderer2) {}
+
+  changeColor() {
+    return this.render.setStyle(this.authorElement.nativeElement,'color','green')
+  }
+
+  ngAfterViewInit() {
+    console.log(this.authorElement);
+  }
 
   getActiveClass() {
     return ['isActive'];
   }
-  getColor(){
-    return this.isActive?'pink':'green';
+  // getColor(){
+  //   return this.isActive?'pink':'green';
+  // }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log(this.authorElement);
+  }
+  getColor(element: HTMLDivElement) {
+    // console.log(element);
+    element.style.color = 'pink';
+    element.style.fontSize = '40px';
   }
 }
