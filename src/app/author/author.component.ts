1a2cd45d8a157e9e3d504dea5a5454d3a2739704
Chandan1154 <chandan18cse@gmail.com>
2024-01-03 13:01:16 +0530

    ngstyle directive

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -3,12 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
-  styleUrl: './author.component.css'
+  styleUrl: './author.component.css',
 })
 export class AuthorComponent {
-   
-  isActive : string  = "is_active"
-  getActiveClass(){
-    return ['isActive']
+  // isActive : string  = "is_active"
+  isActive: boolean = false;
+
+  getActiveClass() {
+    return ['isActive'];
+  }
+  getColor(){
+    return this.isActive?'pink':'green';
   }
 }
