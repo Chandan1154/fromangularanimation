afaaa91129bf1dbf8b215189a07079f51a9e21a5
Chandan1154 <chandan18cse@gmail.com>
2024-01-03 12:39:28 +0530

    ng class class Attribute

--- src/app/author/author.component.ts
+++ src/app/author/author.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './author.component.css'
 })
 export class AuthorComponent {
-
+   
+  isActive : string  = "is_active"
+  getActiveClass(){
+    return ['isActive']
+  }
 }
